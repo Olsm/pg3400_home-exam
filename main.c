7a8f90df7f89930d06f4f4bd650d081a53dd975e
Olsm <OlavOlsm@gmail.com>
2016-11-30 21:28:29 +0100

    store players in players array

--- main.c
+++ main.c
@@ -2,24 +2,21 @@
 #include "board.h"
 
 // TODO: Prevent string overflow
-void createPlayers(char *player1, char *player2) {
-    printf("Enter name player1: ");
-    scanf("%s", player1);
-    printf("Enter name player2: ");
-    scanf("%s", player2);
+void createPlayers(char players[2][20]) {
+    for (int i = 0; i < 2; ++i) {
+        printf("Enter name player1: ");
+        scanf("%s", players[i]);
+    }
     printf("\n\n");
 }
 
 int main(void) {
     // Create players
-    char player1[20];
-    char player2[20];
-    createPlayers(player1, player2);
+    char players[2][20];
+    createPlayers(players);
 
     // Create and print the board
     Board currentBoard;
     initBoard(&currentBoard);
     printBoard(&currentBoard);
-
-    
 }
