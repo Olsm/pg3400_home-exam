b358ff4d2060f1304b0b41edfd5f8f5ffeced253
Olsm <OlavOlsm@gmail.com>
2016-11-30 22:17:08 +0100

    get game input (fields) from players, and avoid string overflows

--- main.c
+++ main.c
@@ -1,22 +1,39 @@
 #include <stdio.h>
 #include "board.h"
 
-// TODO: Prevent string overflow
-void createPlayers(char players[2][20]) {
+void flushBuffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) { }
+}
+
+void createPlayers(char players[2][21]) {
     for (int i = 0; i < 2; ++i) {
-        printf("Enter name player1: ");
-        scanf("%s", players[i]);
+        printf("Enter name player%d (max 20 chars): ", i);
+        scanf("%20s", players[i]);  // Read max 10 chars
+        flushBuffer();
     }
     printf("\n\n");
 }
 
+void playGame(Board board, char players[2][21]) {
+    char field[2];
+    for (int i = 0; i < 2; ++i) {
+        printBoard(&board);
+        printf("Player %s, enter field: ", players[i]);
+        scanf("%2s", field);
+        flushBuffer();
+    }
+}
+
 int main(void) {
     // Create players
-    char players[2][20];
+    char players[2][21];
     createPlayers(players);
 
-    // Create and print the board
+    // Create the board
     Board currentBoard;
     initBoard(&currentBoard);
-    printBoard(&currentBoard);
+
+    // Start the game
+    playGame(currentBoard, players);
 }
