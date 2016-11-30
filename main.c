a8071fec5afebc1e16b6c90066d0c430870d1e99
Olsm <OlavOlsm@gmail.com>
2016-11-30 21:13:50 +0100

    create players

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
+void createPlayers(char *player1, char *player2) {
+    printf("Enter name player1: ");
+    scanf("%s", player1);
+    printf("Enter name player2: ");
+    scanf("%s", player2);
+}
 
+int main(void) {
+    char player1[20];
+    char player2[20];
+    createPlayers(player1, player2);
+    printf("%s %s", player1, player2);
 }
